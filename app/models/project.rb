bfbf6e19a33bab570c14bf82c45d4125562b7312
gene5ive <jedigene@gmail.com>
2015-07-10 14:03:08 -0700

    add bootstrap navbar and fix accessible attributes.

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,7 @@ class Project < ActiveRecord::Base
   belongs_to :skill
 
   validates :name, :description, :presence => true
+
+  attr_accessible :name, :description, :project_link
+
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -2,4 +2,6 @@ class Skill < ActiveRecord::Base
   has_many :projects
 
   validates :name, :description, :presence => true
+
+  attr_accessible :name, :description
 end
