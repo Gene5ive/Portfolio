96dec0667bc93f99b681e9fbfce7596e27b6345c
gene5ive <jedigene@gmail.com>
2015-07-24 13:17:24 -0700

    rebase db and fix spec.

--- spec/models/recommendation_spec.rb
+++ spec/models/recommendation_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe Reference do
+describe Recommendation do
   it { should validate_presence_of :body }
   it { should validate_presence_of :name }
   it { should validate_presence_of :company }
