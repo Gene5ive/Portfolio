1659e4bd5324cad42349aff6a28d3a7a8f0c2364
Gene Johnson <jedigene@gmail.com>
2023-04-18 13:41:32 -0700

    Update application_controller.rb
    Use before_action instead of before_filter: In Rails 5 and later versions, before_filter has been deprecated in favor of before_action. So, you should update the before_filter line to before_action for consistency and future-proofing.
    
    Use :null_session instead of :exception for CSRF protection: In the protect_from_forgery line, you are currently using :exception as the option for CSRF protection. However, using :null_session is generally recommended for APIs, as it doesn't raise an exception and instead clears the session data. This can help prevent potential CSRF issues while still allowing API requests to be processed.
    
    Consider using helper selectively: The line commented out # helper :all includes all helpers in your application, which can add unnecessary overhead. Instead, you can include only the helpers that are needed in each individual controller or view, using the helper method with specific helper modules.
    
    Remove commented-out code: The lines commented out with # include ActionView::Helpers::NumberHelper seem to be unused, so you can remove them to keep the code clean and reduce potential confusion.
    
    Use find_by instead of find to avoid exceptions: In the current_user method, you are using User.find(session[:user_id]) to find the current user. However, if the user with the given user_id doesn't exist, it will raise an exception. To avoid this, you can use find_by_id instead, which returns nil if the user is not found, like this: User.find_by_id(session[:user_id]).

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,9 @@
 class ApplicationController < ActionController::Base
-  before_filter :authenticate_user!, except: [:index, :show]
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-  # helper :all # include all helpers, all the time
-  # include ActionView::Helpers::NumberHelper
+  before_action :authenticate_user!, except: [:index, :show]
+  protect_from_forgery with: :null_session
 
   def current_user
     return unless session[:user_id]
-    @current_user ||= User.find(session[:user_id])
+    @current_user ||= User.find_by_id(session[:user_id])
   end
-  
 end
