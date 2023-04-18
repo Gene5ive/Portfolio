0922b796b9cb8136f16048b592dc638ee6fc10c8
Gene Johnson <jedigene@gmail.com>
2023-04-18 12:29:32 -0700

    Update blogs_controller.rb

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,11 +1,13 @@
 class BlogsController < ApplicationController
+  # Use before_action to set @blog instance variable for show, edit, update, and destroy actions
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
 
   def index
     @blogs = Blog.all
   end
 
   def show
-    @blog = Blog.find(params[:id])
+    # @blog instance variable already set by set_blog method
   end
 
   def new
@@ -27,11 +29,10 @@ class BlogsController < ApplicationController
   end
 
   def edit
-    @blog = Blog.find(params[:id])
+    # @blog instance variable already set by set_blog method
   end
 
   def update
-    @blog = Blog.find(params[:id])
     if @blog.update(blog_params)
       flash[:notice] = "Blog successfully edited!"
     else
@@ -45,7 +46,6 @@ class BlogsController < ApplicationController
   end
 
   def destroy
-    @blog = Blog.find(params[:id])
     @blog.destroy
     flash[:notice] = "Blog successfully deleted!"
     redirect_to blogs_path
@@ -54,5 +54,9 @@ class BlogsController < ApplicationController
   private
   def blog_params
     params.require(:blog).permit(:title, :body)
+  end
+
+  def set_blog
+    @blog = Blog.find(params[:id])
   end
 end
